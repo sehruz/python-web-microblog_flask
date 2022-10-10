374035d175ce52a48c861b995ca77abaf41ae4b0
sehruz <92758383+sehruz@users.noreply.github.com>
2022-10-10 23:24:12 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 from pymongo import MongoClient
+def create_app():
+    app=Flask(__name__)
+    client=MongoClient('mongodb+srv://sehruz787:Boeing787320@microblog-app.3ef2ggs.mongodb.net/test')
+    app.db=client.microblog
+    entries=[]
 
-app=Flask(__name__)
-client=MongoClient('mongodb+srv://sehruz787:Boeing787320@microblog-app.3ef2ggs.mongodb.net/test')
-app.db=client.microblog
-entries=[]
+    @app.route('/', methods=['GET', 'POST'])
+    def home():
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    
-    if request.method == 'POST':
-        entry_content=request.form.get('content')
-        entry_time=datetime.today().strftime('%Y-%m-%d')
-        if entry_content !="":
-            # entries.append((entry_content, entry_time))
-            app.db.entries.insert_one({'content': entry_content, 'date': entry_time})
-    
-    entry_with_date=[(entry['content'], entry['date'],
-    datetime.strptime(entry['date'], '%Y-%m-%d').strftime('%b %d')
-    ) for entry in app.db.entries.find({})]
-    return render_template('home.html', entries=entry_with_date)
+        if request.method == 'POST':
+            entry_content=request.form.get('content')
+            entry_time=datetime.today().strftime('%Y-%m-%d')
+            if entry_content !="":
+                # entries.append((entry_content, entry_time))
+                app.db.entries.insert_one({'content': entry_content, 'date': entry_time})
+
+        entry_with_date=[(entry['content'], entry['date'],
+        datetime.strptime(entry['date'], '%Y-%m-%d').strftime('%b %d')
+        ) for entry in app.db.entries.find({})]
+        return render_template('home.html', entries=entry_with_date)
+    return app
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
